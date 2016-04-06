3fbd7e91f314055818b47a8b7a4a54aea9f91cdc
ChrisAlatorre <cjalatorre@gmail.com>
2016-04-05 19:40:24 -0700

    ioConnection listener update. Need to test

---
+++ server/handlers/ioConnection.js
@@ -0,0 +1,38 @@
+export default function(clients, mainRoom) {
+    return function(socket) {
+        clients[socket.id] = socket;
+        mainRoom.chatterCount++;
+        mainRoom.chatters.push(socket);
+        socket.join(mainRoom.name);
+        //Error Handler
+        socket.on('error', function (err) {
+            console.log("Error %s", err);
+        });
+        //Disconnect Handler
+        socket.on('disconnect', function() {
+            delete cliets[socket.id];
+            let slot = mainRoom.chatters.findIndex((s)=> {
+                if(s === socket) return true;
+                else return false;
+            });
+            mainRoom.chatters.splice(slot, 1);
+            mainRoom.chatterCount--;
+        });
+        //Messages from main room
+        socket.on('mainRoom-msg', function(data) {
+            socket.broadcast.to(mainRoom.name).emit('message',msg);
+        });
+        //
+
+        /*
+        socket.on('peer-msg', function(data) {
+            console.log('Message from peer: %s', data);
+            socket.broadcast.emit('peer-msg', data);
+        });
+
+        socket.on('go-private', function(data) {
+            socket.broadcast.emit('go-private', data);
+        });
+        */
+    }
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,15 @@
 import Path from 'path';
 import Inert from 'inert';
+import ioConnection from '../handlers/ioConnection';
 import fs from 'fs';
 //
 export default function(server, io) {
+    const clients = [];
+    const mainRoom = {chatters: [], chatterCount: 0, name:'mainRoom'};
+    //
     server.register(Inert, () => {});
+    //
+    io.on('connection', ioConnection(clients, mainRoom));
     //
     server.route([
         //Serve Files out of the static folder
